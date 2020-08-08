724e9b414421de67a5457537c0412fc8ab8fa190
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-08 16:14:47 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+def reverse_each_word(string)
+  words = string.split("")
+  new_words = []
+  new_sent = []
+  
+  words.each do |words|
+    p words
+    new_words.unshift(words)
+  end
+  reversed = new_words.join("")
+  reversed_words = reversed.split(" ")
+  
+  reversed_words.collect do |word|
+    new_sent.unshift(word)
+  end
+  
+  new_sent.join(" ")
+end
