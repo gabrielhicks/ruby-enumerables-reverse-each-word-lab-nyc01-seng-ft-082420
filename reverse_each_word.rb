2adec0221ba3224ebc2a1f5362e92750b548740b
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-08 16:23:13 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,28 @@
+# def reverse_each_word(string)
+#   words = string.split("")
+#   new_words = []
+#   new_sent = []
+  
+#   words.each do |words|
+#     p words
+#     new_words.unshift(words)
+#   end
+#   reversed = new_words.join("")
+#   reversed_words = reversed.split(" ")
+  
+#   reversed_words.each do |word|
+#     new_sent.unshift(word)
+#   end
+  
+#   new_sent.join(" ")
+# end
+
 def reverse_each_word(string)
-  words = string.split("")
-  new_words = []
-  new_sent = []
+  words = string.split(" ")
   
-  words.each do |words|
-    p words
-    new_words.unshift(words)
-  end
-  reversed = new_words.join("")
-  reversed_words = reversed.split(" ")
-  
-  reversed_words.collect do |word|
-    new_sent.unshift(word)
+  words.collect do |word|
+    word.reverse!
   end
   
-  new_sent.join(" ")
+   words.join(" ")
 end
